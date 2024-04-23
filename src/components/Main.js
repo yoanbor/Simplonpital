8fd4e5ab066faa626432e4c648c01d67f655b277
YoanBor <yoanbor@icloud.com>
2024-04-23 10:48:18 +0200

    feat(main): Ajout d'un main initial avec son style

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cards from "./Cards";
+
+const Main = () => {
+    return (
+        <div className="main-container">
+            <main>
+                <h2>Patients</h2>
+                <Cards />
+            </main>
+        </div>
+    );
+};
+
+export default Main;
