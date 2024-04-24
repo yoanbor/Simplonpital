664fc35d4c724fbf7568ddcb620b9af8a5373947
YoanBor <yoanbor@icloud.com>
2024-04-24 10:14:26 +0200

    feat, style(Card): La card contenant les patients et leurs infos personnelles avec le style correspondant aux maquettes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 const Card = ({ bed }) => {
-    console.log(bed.room)
     return (
-        <li className="card">
-            <h3>{bed.patient ? bed.patient.firstName : ""}</h3>
-            <h3>{bed.patient ? bed.patient.lastName : ""}</h3>
-            <p>{bed.patient ? bed.patient.birthdate : ""}</p>
-            <p>{bed.patient ? bed.patient.socialSecurityNumber : ""}</p>
-            <h3>{bed.patient ? "Service" : ""}</h3>
-            <p>{bed.patient ? bed.room.service.name : ""}</p>
-            <h3>{bed.patient ? "Chambre" : ""}</h3>
-            <p>{bed.patient ? bed.room.number : ""}</p>
-        </li>
+        <>
+            {bed.patient && (
+                <li className="card">
+                    <div className="card-patient">
+                        <div className="card-nom-prenom">
+                            <h3>{bed.patient.firstName}</h3>
+                            <h3>{bed.patient.lastName}</h3>
+                        </div>
+                        <p>{bed.patient.birthdate}</p>
+                        <p>{bed.patient.socialSecurityNumber}</p>
+                    </div>
+                    <div className="card-service-chambre">
+                        <div className="card-service">
+                            <h3>Service</h3>
+                            <p>{bed.room.service.name}</p>
+                        </div>
+                        <div className="card-chambre">
+                            <h3>Chambre</h3>
+                            <p>{bed.room.number}</p>
+                        </div>
+                    </div>
+                </li>
+            )}
+        </>
     );
 };
 
